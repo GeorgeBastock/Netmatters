5fe9259b65dda04d7db632e0802b3dfb6a53c9e6
George Bastock <42344811+georgebastock@users.noreply.github.com>
2019-06-14 16:10:10 +0100

    Added side menu and bug fixes

--- js/main.js
+++ js/main.js
@@ -3,7 +3,39 @@ $(document).ready(function(){
         dots: true,
         prevArrow: false,
         nextArrow: false,
-        autoplay: false,
+        autoplay: true,
         autoplaySpeed: 3500,
     });
+});
+
+$('.hamburger').on('click', function(e) {
+    $('body').toggleClass("menu-toggle");
+    $('#side-nav').toggleClass("side-menu-push");
+
+    if ($('body').hasClass('menu-toggle')) {
+        $('body').css('right', 350);
+        // $('body').css({'overflow':'hidden'});
+
+        $(document).bind('scroll',function () { 
+            window.scrollTo(0,0); 
+        });
+    } else {
+        $('body').css('right', 0);
+    }
+    e.preventDefault();
+});
+
+$('.side-menu-opacity').on('click', function(e) {
+    $('body').toggleClass("menu-toggle");
+    $('#side-nav').toggleClass("side-menu-push");
+
+    if ($('body').hasClass('menu-toggle')) {
+        $('body').css('right', 350);
+    } else {
+        $('body').css('right', 0);
+
+        $(document).unbind('scroll'); 
+        $('body').css({'overflow':'visible'});
+    }
+    e.preventDefault();
 });
