aac92146a4431cd0a41221056670f4631a9c5806
George Bastock <42344811+georgebastock@users.noreply.github.com>
2019-06-14 12:30:18 +0100

    Added slick carousel to hero section.

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.slickSlider').slick({
+        dots: true,
+        prevArrow: false,
+        nextArrow: false,
+        autoplay: false,
+        autoplaySpeed: 3500,
+    });
+});
